f25707814cfbdc09f13e91398feab2dea658f4d2
Eric Henrique <85954795+Erichenrique77@users.noreply.github.com>
2021-10-18 13:32:11 +0000

    links

--- js/menu.js
+++ js/menu.js
@@ -4,21 +4,5 @@ $(document).ready(function () {
     e.preventDefault()
     $('#menu').toggle('slow')
   })
-
- $('a').click(function(e){
-    e.preventDefault()
-    let url = $(this).attr('href')
-    $('#'+url).show()
-
-    if (id == '#home') {
-      $('home').toggle();
-      $('acao').hide(1000);
-      $('sobre').hide(1000);
-      $('contact').hide(1000);
-      
-
-
-      }
-
-  })
+  
 }) 
